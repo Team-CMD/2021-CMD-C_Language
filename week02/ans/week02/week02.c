8c7ee24335a8fe56d266a9d22044f16a636ad747
JH9892 <jhchoi09@naver.com>
2021-11-02 21:03:49 +0900

    upload two assignments

--- week02/ans/week02/week02.c
+++ week02/ans/week02/week02.c
@@ -1,6 +1,20 @@
-﻿#include <stdio.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
 
-int main(){
-    printf("Hello Wolrd!\n");
+int main() {
+    char arr[7];
+    scanf_s("입력 : %s", arr);
+
+    int i;
+    for (i = 0; i < 7; i++) {
+        printf("%c ", arr[i]);
+    }
+
+    if (arr[0] == arr[6] && arr[1] == arr[5] && arr[2] == arr[4]) {
+        printf("this is palindrome!!");
+    }
+    else {
+        printf("this is not palindrome...");
+    }
     return 0;
 }
