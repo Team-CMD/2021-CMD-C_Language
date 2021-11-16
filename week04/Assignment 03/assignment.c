02978a84904fe8e04e089a150a7d6e95336bdd89
JH9892 <jhchoi09@naver.com>
2021-11-16 18:09:03 +0900

    upload reference week04

---
+++ week04/Assignment 03/assignment.c
@@ -0,0 +1,38 @@
+# include <stdio.h>
+# define SIZE 10
+
+char stack[SIZE];
+int top=-1;
+
+int isFull(){ // 스택이 꽉 찼는지 아닌지를 검사하는 함수 
+	
+}
+
+int isEmpty(){ // 스택이 비어있는지 아닌지를 검사하는 함수 
+	
+}
+
+void push(char data){// 스택에 data를 삽입하는 함수 
+	
+}
+
+char pop(){// 스택에 원소를 제거하는 함수 
+	
+}
+
+char peek(){// 스택에 맨 위 원소를 반환하는 함수 
+	
+}
+
+void printStack(){// 스택의 모든 원소를 출력하는 함수 
+	
+}
+
+int main(){
+	// Please Write your code about "Singly List" 
+
+	// Please Write your code about "Stack"
+	
+	return 0;
+}
+
